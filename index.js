941e6357a4c7995811bf1a184757ea53ad36b87f
Aashish Lamsal <aashish33lamsal@gmail.com>
2025-11-06 00:01:08 +0545

    cors added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const mongoose =  require('mongoose')
 const app = express()
 const port = 6000
@@ -11,7 +12,13 @@ dotenv.config();
 console.log(`from env ${process.env.PORT}`)
 
 mongoose.connect(process.env.MONGOURL).then(()=>console.log("db connected")).catch((err)=>console.log(err));
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "*",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use('/api',authRouter);
 app.use("/hello", (req, res)=>{
